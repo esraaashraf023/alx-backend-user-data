0ba857e1cdbfbffcec653451741feaf9f281c4b6
esraaashraf023 <youesraaashraf780567@gmail.com>
2024-05-24 22:38:48 +0300

    add files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class to manage the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method require_auth"""
+        if path is None or not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+
+        path = path + '/' if not path.endswith('/') else path
+
+        for excl_path in excluded_paths:
+            if excl_path.endswith('*'):
+                norm_excl_path = excl_path[:-1]
+                if path.startswith(norm_excl_path):
+                    return False
+            else:
+                excl_path = excl_path + '/' if not\
+                    excl_path.endswith('/') else excl_path
+                if path == excl_path:
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """public method authorization_header"""
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method current_user"""
+        return None
